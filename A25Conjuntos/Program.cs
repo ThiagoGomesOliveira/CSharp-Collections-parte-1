23e630fc79268fe2ac5d8ef5ff594a7d7d25aee3
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2019-04-20 06:36:08 -0300

    Conjuntos Poder do SET pt2

---
+++ A25Conjuntos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A25Conjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISet<string> alunos = new HashSet<string>();
+
+            alunos.Add("Thiago Gomes Oliveira");
+            alunos.Add("Juliana Leoni");
+            alunos.Add("Luiz Fernando Gomes de Oliveira");
+
+            Console.WriteLine(alunos);
+            Imprimir(alunos);
+
+            // adcionando aluno novo para verificar a ordem do SET
+            alunos.Add("Gustavo Gomes");
+            Imprimir(alunos);
+            // Removendo e adcionando aluno no SET
+            alunos.Remove("Gustavo Gomes");
+            alunos.Add("Pedro Henrique");
+            Imprimir(alunos);
+            //Adcionando o mesmo aluno no SET
+            alunos.Add("Juliana Leoni");
+            Imprimir(alunos);
+
+            //ordernando  o SET
+            List<string> listaAlunos = new List<string>(alunos);
+            listaAlunos.Sort();
+            Console.WriteLine(string.Join(", ",listaAlunos));
+ 
+        }
+
+
+        public static void Imprimir(ISet<string> conjuntos)
+        {
+            Console.WriteLine(string.Join(", ",conjuntos));
+        }
+    }
+}
