ac77bfff5371b57a147ebbf6a823348aae0a8607
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2019-04-28 14:02:46 -0300

    Queue

---
+++ A29Queue/Pedagio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A29Queue
+{
+    public class Pedagio
+    {
+        private Queue<string> veiculos = new Queue<string>();
+
+        public void Enfileirar(string veiculo)
+        {
+            Console.WriteLine($"Entrou na fila {veiculo}");
+            veiculos.Enqueue(veiculo);
+            ListarFilaPedagio();
+        }
+
+        private void ListarFilaPedagio()
+        {
+            Console.WriteLine("FILA: ");
+            foreach (var v in this.veiculos)
+            {
+                Console.WriteLine(v);
+               
+            }
+            Console.WriteLine();
+        }
+
+        public void Desfileirar()
+        {
+            if (this.veiculos.Any())
+            {
+                // verificar  veículo seguinte sem que ele seja removido
+                if (this.veiculos.Peek() == "guincho")
+                {
+                    Console.WriteLine("Guincho está efetuando o pagamento");
+                }
+
+                Console.WriteLine($"Saiu da fila {this.veiculos.Dequeue()}");
+                ListarFilaPedagio();
+            }
+          
+        }
+
+    }
+}
---
+++ A29Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A29Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pedagio pedagio = new Pedagio();
+
+            pedagio.Enfileirar("gol");
+            pedagio.Enfileirar("Fusca");
+            pedagio.Enfileirar("guincho");
+
+            ICollection<Pedagio> alunos = new HashSet<Pedagio>();
+            pedagio.Desfileirar();
+        }
+    }
+}
