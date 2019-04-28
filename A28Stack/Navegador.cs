e8d9a89841823de468b03fd7a7228367f23b88b4
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2019-04-28 09:49:55 -0300

    Stack - aprendendo sobre pilhas.

---
+++ A28Stack/Navegador.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A28Stack
+{
+    public class Navegador
+    {
+        private string atual;
+        private readonly Stack<string> historicoAnterior = new Stack<string>();
+        private readonly Stack<string> historicoProximo = new Stack<string>();
+
+        public Navegador()
+        {
+            this.atual = "vazia";
+            Console.WriteLine("Página atual: " + this.atual);
+        }
+
+        internal void NavegarPara(string url)
+        {
+            historicoAnterior.Push(atual);
+            this.atual = url;
+            Console.WriteLine("Página atual: "+this.atual);
+        }
+
+        internal void Anterior()
+        {
+            if (this.historicoAnterior.Any())
+            {
+                historicoProximo.Push(this.atual);
+                this.atual = this.historicoAnterior.Pop();
+                Console.WriteLine("Página atual: " + this.atual);
+            };
+        }
+
+        internal void Proxima()
+        {
+            if (historicoProximo.Any())
+            {
+                this.historicoAnterior.Push(this.atual);
+                this.atual = historicoProximo.Pop();
+                Console.WriteLine("Página atual: " + this.atual);
+            }
+        }
+
+        
+
+    }
+}
---
+++ A28Stack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A28Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Navegador navegador = new Navegador();
+
+            navegador.NavegarPara("google.com");
+            navegador.NavegarPara("caelum.com.br");
+            navegador.NavegarPara("Alura.com.br");
+
+            Console.WriteLine("===============================");
+
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+
+            Console.WriteLine("===============================");
+
+            navegador.Proxima();
+            navegador.Proxima();
+            navegador.Proxima();
+
+        }
+    }
+}
